81422b29d6a17d38ba9aef64856d6bd705cb8624
srajalingam <rajalingam299@gmail.com>
2026-04-02 00:44:56 +0530

    Create template from server

--- Web_app/cmd/web/handlers.go
+++ Web_app/cmd/web/handlers.go
@@ -3,5 +3,10 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Virtual Terminal")
+	//app.infoLog.Println("Virtual Terminal")
+
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
