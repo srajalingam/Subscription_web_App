d105936bb237427ca75f1b587c2f4447466fd97e
srajalingam <rajalingam299@gmail.com>
2026-04-17 00:00:54 +0530

    Required model created for Customers & Orders

--- Web_app/internal/models/models.go
+++ Web_app/internal/models/models.go
@@ -39,6 +39,7 @@ type Order struct {
 	ID            int    `json:"id"`
 	WidgetID      int    `json:"widget_id"`
 	TransactionID int    `json:"transaction_id"`
+	CustomerId    int    `json:"customer_id"`
 	StatusID      int    `json:"status_id"`
 	Quantity      int    `json:"quantity"`
 	Amount        int    `json:"amount"`
@@ -70,6 +71,15 @@ type User struct {
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+type Customer struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
