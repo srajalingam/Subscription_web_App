ccef457ca31f0dc4f747fe04d33d3dc69d456c06
srajalingam <rajalingam299@gmail.com>
2026-04-23 00:16:43 +0530

    Handler for POST Request

--- Web_app/cmd/api/handlers-api.go
+++ Web_app/cmd/api/handlers-api.go
@@ -11,8 +11,12 @@ import (
 )
 
 type stripePayload struct {
-	Currency string `json:"currency"`
-	Amount   string `json:"amount"`
+	Currency      string `json:"currency"`
+	Amount        string `json:"amount"`
+	PaymentMethod string `json:"payment_method"`
+	Email         string `json:"email"`
+	LastFour      string `json:"last_four"`
+	Plan          string `json:"plan"`
 }
 
 type jsonResponse struct {
@@ -97,6 +101,38 @@ func (app *application) GetWidgetByID(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func (app *application) CreateCustomerAndSubscribeToPlan(w http.ResponseWriter, r *http.Request) {
+	var data stripePayload
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		http.Error(w, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+	app.infoLog.Println(data)
+
+	okay := true
+	msg := ""
+
+	resp := jsonResponse{
+		OK:      okay,
+		Message: msg,
+		Content: "",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "  ")
+
+	if err != nil {
+		app.errorLog.Println(err)
+		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
 }
