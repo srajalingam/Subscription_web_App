801a8dc6b352884c502aa265eedfe4e2cefa015f
srajalingam <rajalingam299@gmail.com>
2026-05-16 23:51:46 +0530

    create the function to generate the token

---
+++ Web_app/internal/models/tokens.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
+
+type Token struct {
+	PlainText string `json:"token"`
+	UserID    int64  `json:"-"`
+	Hash      []byte `json:"-"`
+	Expiry    int64  `json:"expiry"`
+	Scope     string `json:"scope"`
+}
+
+func GenerateToken(userID int, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: int64(userID),
+		Expiry: time.Now().Add(ttl).Unix(),
+		Scope:  scope,
+	}
+
+	randBytes := make([]byte, 16)  // Generate 16 random bytes
+	_, err := rand.Read(randBytes) // Fill the byte slice with random data
+	if err != nil {
+		return nil, err
+	}
+	token.PlainText = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randBytes) // Encode the random bytes to a base32 string without padding and padding for URL safety
+	hash := sha256.Sum256([]byte(token.PlainText))                                               // Hash the plaintext token using SHA-256
+	token.Hash = hash[:]                                                                         // Store the hash in the token struct
+
+	return token, nil
+}
